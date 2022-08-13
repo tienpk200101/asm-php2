7a62e1420b0dcb25f9487cdfd266ae9d6a550cdf
tienpk <phungkhactien2001@gmail.com>
2022-08-13 13:51:14 +0700

    update

--- app/Http/Controllers/Frontend/FrontEndController.php
+++ app/Http/Controllers/Frontend/FrontEndController.php
@@ -20,7 +20,7 @@ class FrontEndController extends Controller
     public function index(){
         $banners = Banner::all();
         $prorduct = new Product();
-        $prorducts = $prorduct->loadListWithPage('',1);
+        $prorducts = $prorduct->loadListWithPage('',15);
         $categories = new Category();
         $this->v['categories'] = $categories->getData();
         $this->v['products'] = $prorducts;
@@ -32,7 +32,7 @@ class FrontEndController extends Controller
     public function shopPage(){
         $this->v['title'] = 'Cửa hàng';
         $prorduct = new Product();
-        $prorducts = $prorduct->loadListWithPage(12);
+        $prorducts = $prorduct->loadListWithPage('', 12);
         $this->v['products'] = $prorducts;
         return view('frontend.shoppage', $this->v);
     }
